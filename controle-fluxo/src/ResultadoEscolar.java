f39d735cbf96cef1d5285a152606d7fa5e2751b6
Elaine5 <lanne9873@gmail.com>
2022-10-14 13:30:43 -0300

    Att

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -3,7 +3,23 @@
  */
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        
+        // Estruturas condicionais
+        /** int nota = 6;
+
+        if(nota >= 7)
+        System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota <7)
+        System.out.println("Prova de Recuperaçõa");
+
+        else
+        System.out.println("Reprovado");        */
+
+        // Condições ternárias
+
+        int nota = 3;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);            
     }
     
 }
