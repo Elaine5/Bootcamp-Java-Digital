ce916c29aca7367549f9e6d9196b0646a1d01d04
Elaine5 <lanne9873@gmail.com>
2022-10-17 15:13:07 -0300

    about me

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
+
+        
+    }
+}
